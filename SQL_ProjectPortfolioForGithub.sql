4895c570282b6f4060096807e83cf6cf779f675c
Laurieastremon <136273359+Laurieastremon@users.noreply.github.com>
2023-06-11 14:05:16 -0700

    Add files via upload

---
+++ SQL_ProjectPortfolioForGithub.sql
@@ -0,0 +1,11 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [PortfolioProject].[dbo].[PercentPopVaccinated]
+
+
+SELECT*
+From PercentPopVaccinated
